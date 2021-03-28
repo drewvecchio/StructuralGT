ed0acb1ee108740bff849ca8d9a58aac0dec0c08
drewvecchio <81522502+drewvecchio@users.noreply.github.com>
2021-03-28 14:08:22 -0400

    Delete venv/lib/python3.8/site-packages directory

--- venv/lib/python3.8/site-packages/cvxpy/utilities/deterministic.py
+++
@@ -1,9 +0,0 @@
-def unique_list(duplicates_list):
-    """Return unique list preserving the order.
-    https://stackoverflow.com/a/58666031/1002277
-    """
-    used = set()
-    unique = [x for x in duplicates_list
-              if x not in used and
-              (used.add(x) or True)]
-    return unique
--- venv/lib/python3.8/site-packages/networkit/gephi/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import streaming 
-from .csv import exportNodeValues
-
-# hide those modules from the user
-del pyclient
-del csv
