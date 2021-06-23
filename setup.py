29da090a45c7a827bae42cb12ce531bc25b953a8
drewvecchio <81522502+drewvecchio@users.noreply.github.com>
2021-06-23 19:47:01 -0400

    Bump version
    Signed-off-by: drewvecchio <81522502+drewvecchio@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ Contact email: vecdrew@umich.edu
 
 setup(
     name='StructuralGT',
-    version='1.0.1a2',
+    version='1.0.1a3',
     packages=find_packages(),
     url='https://github.com/drewvecchio/StructuralGT',
     license='GNU General Public License v3',
