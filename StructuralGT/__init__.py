30d1efab93d16da268ae37a81b55996de13f9e29
drewvecchio <81522502+drewvecchio@users.noreply.github.com>
2021-06-23 21:25:36 -0400

    Bumped to beta version
    Signed-off-by: drewvecchio <81522502+drewvecchio@users.noreply.github.com>

--- StructuralGT/__init__.py
+++ StructuralGT/__init__.py
@@ -5,6 +5,6 @@ A python package for automated graph theory analysis
 of digital structural network images.
 """
 
-__version__ = "1.0.1a4"
+__version__ = "1.0.1b0"
 __author__ = "Drew Vecchio"
 __credits__ = "The Regents of the University of Michigan"
